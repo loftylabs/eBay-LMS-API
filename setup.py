94b89deee3d3993568382a3302eff45997c4f137
Casey Kinsey <casey.kinsey@gmail.com>
2017-12-26 12:17:27 -0600

    Convert to distutils package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='lmslib',
+    version='0.1',
+    description='Python Bindings for eBay LMS API. ',
+    author='Wesley Hansen',
+    author_email='casey@hirelofty.com',
+    url='https://github.com/loftylabs/',
+    packages=['lmslib'],
+)
